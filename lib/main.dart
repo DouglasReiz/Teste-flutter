486a28e322ab2e9260ef282713a7f95bc2a6c807
Douglas Alves <douglasreisalves02@gmail.com>
2022-09-29 18:05:01 -0300

    novos testes com flutter (app bar e Bottom navigationBar)

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: MyWidget(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App Bar'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      drawer: const Drawer(),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+            label: 'Settings'
+          )
+        ],
+      ),
+
+
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-    var _selectedIndex = 0;
-
-    static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-
-    static const List<Widget> _widgetOptions = <Widget>[
-      Text(
-        'Index 0: Home',
-        style: optionStyle,
-      ),
-
-      Text(
-        'Index 1: Business',
-        style: optionStyle,
-      ),
-
-      Text(
-        'Index 2: School',
-        style: optionStyle,
-      ),
-    ];
-
-    void _onItemTapped(int index) {
-      setState(() {
-        _selectedIndex = index;
-      });
-    }
-
-    @override
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Meu teste com bottom navbar'),
-        ),
-        body: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.hub_outlined),
-              label: 'Home',
-            ),
-
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business_outlined),
-              label: 'Business',
-            ),
-
-            BottomNavigationBarItem(
-              icon: Icon(Icons.share_outlined),
-              label: 'School',
-            ),
-
-          ],
-
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.amber[800],
-          onTap: _onItemTapped,
-
-        ),
-      );
-    }
-
-  }
-
-
